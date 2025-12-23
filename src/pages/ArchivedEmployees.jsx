62da49499cd1003174db46f9ca1e2aeff82c2ce7
theodan11 <theophiljadango@gmail.com>
2025-12-23 20:53:19 +0600

    infinite bug fix

--- src/pages/ArchivedEmployees.jsx
+++ src/pages/ArchivedEmployees.jsx
@@ -1,9 +1,10 @@
 import EmployeeTableSort from '../components/EmployeeTableSort'
 import DrawerCom from '../components/DrawerCom'
+import './archiveEmployees.css'
 
 const ArchivedEmployees = () => {
     return (
-        <div>
+        <div className='archive__container'>
             <h2>Archived Employees</h2>
             <EmployeeTableSort isArchive={true} />
             <DrawerCom />
